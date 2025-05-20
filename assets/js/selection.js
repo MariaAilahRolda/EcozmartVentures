78302f7d50d09e2afa10eb76051a51cd8420b21d
MariaAilahRolda <roldamariaailah@gmail.com>
2025-05-20 16:41:25 +0800

    05/20/2025

---
+++ assets/js/selection.js
@@ -0,0 +1,21 @@
+ // Color swatch click -> image switch
+  const colorMap = {
+    PUWhite_10: "assets/img/products/OJO/PUWhite_10.png",
+    PUGrey_10: "assets/img/products/OJO/PUGrey_10.png",
+
+    PUWhite_4: "assets/img/products/OJO/PUWhite_4.png",
+    PUGrey_4: "assets/img/products/OJO/PUGrey_4.png"
+  };
+
+  document.querySelectorAll(".color-swatch").forEach(swatch => {
+    swatch.addEventListener("click", () => {
+      const colorKey = swatch.getAttribute("data-color");
+      const newSrc = colorMap[colorKey];
+      if (newSrc) {
+        productImage.src = newSrc;
+        lens.style.backgroundImage = `url('${newSrc}')`;
+      } else {
+        console.error("No image found for:", colorKey);
+      }
+    });
+  });
---
+++ assets/js/zoom.js
@@ -0,0 +1,37 @@
+const image = document.getElementById("productImage");
+  const container = image.parentElement;
+
+  const lens = document.createElement("div");
+  lens.classList.add("lens");
+  container.appendChild(lens);
+
+  container.addEventListener("mousemove", moveLens);
+  container.addEventListener("mouseenter", () => lens.style.display = "block");
+  container.addEventListener("mouseleave", () => lens.style.display = "none");
+
+  function moveLens(e) {
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const lensSize = lens.offsetWidth / 2;
+    const left = x - lensSize;
+    const top = y - lensSize;
+
+    lens.style.left = `${left}px`;
+    lens.style.top = `${top}px`;
+
+    const scale = 2;
+    const bgX = -(x * scale - lens.offsetWidth / 2);
+    const bgY = -(y * scale - lens.offsetHeight / 2);
+
+    lens.style.backgroundImage = `url('${image.src}')`;
+    lens.style.backgroundSize = `${image.width * scale}px ${image.height * scale}px`;
+    lens.style.backgroundPosition = `${bgX}px ${bgY}px`;
+  }
+
+  
+  
+  
+
+ 
