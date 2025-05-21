b3afbc974b8605ea248e38793e334fab24afb938
MariaAilahRolda <146386859+MariaAilahRolda@users.noreply.github.com>
2025-05-21 23:04:10 +0800

    5/21

--- assets/js/selection.js
+++ assets/js/selection.js
@@ -8,7 +8,17 @@
 
     MultiBlack: "assets/img/products/OJO/MultiBlack.png",
     MultiTrans: "assets/img/products/OJO/MultiTrans.png",
-    MultiWhite: "assets/img/products/OJO/MultiWhite.png"
+    MultiWhite: "assets/img/products/OJO/MultiWhite.png",
+
+    Vinyl_1: "assets/img/products/OJO/VinylTile_1.5.png",
+    Vinyl_2: "assets/img/products/OJO/VinylTile_3.png",
+
+    WallPutty_1: "assets/img/products/OJO/WallPutty_1.5.png",
+    WallPutty_2: "assets/img/products/OJO/WallPutty_3.png",
+
+    LeaksWhite: "assets/img/products/OJO/LeaksWhite.png",
+    LeaksClear: "assets/img/products/OJO/LeaksClear.png",
+    LeaksGrey: "assets/img/products/OJO/LeaksGrey.png"
 
   };
 
@@ -21,6 +31,19 @@
         lens.style.backgroundImage = `url('${newSrc}')`;
       } else {
         console.error("No image found for:", colorKey);
+      }
+    });
+  });
+document.querySelectorAll('.color-btn').forEach(button => {
+    button.addEventListener("click", () => {
+      const colorKey = button.getAttribute("data-color");
+      const newSrc = colorMap[colorKey];
+
+      if (newSrc) {
+        productImage.src = newSrc;
+        lens.style.backgroundImage = `url('${newSrc}')`;
+      } else {
+        console.error("No image mapped for", colorKey);
       }
     });
   });
