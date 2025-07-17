e5aafca530d2491ee429770223fce2233fe9d729
MariaAilahRolda <146386859+MariaAilahRolda@users.noreply.github.com>
2025-07-17 16:16:49 +0800

    script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function sendMail() {
   emailjs.send("service_rqa9c3n", "template_d46nlpj", parms)
     .then(function(response) {
       alert("✅ Email sent successfully!");
-      document.getElementById("contact-form").reset(); // Optional: clear form
+      document.getElementById("contact-form").reset();
     }, function(error) {
       alert("❌ Failed to send email: " + error.text);
     });
